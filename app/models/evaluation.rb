7c89371d893c9618b9949506f95a0e9a7bf97d03
YukiSato <yukiyukiamam@gmail.com>
2019-06-26 22:39:30 +0900

    ユーザー名が空でも許容するように変更

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -5,7 +5,7 @@ class Evaluation < ApplicationRecord
   validates :timetable_id, numericality: true
   validates :star, numericality: true
   validates :title, presence: true
-  validates :user, presence: true
+  #validates :user, presence: true
   #validates :permission, presence: true
 
 end
